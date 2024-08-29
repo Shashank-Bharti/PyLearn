807e3c6d6706ddd4a95a7b146d4c631d901ffbf6
Shashank Bharti <shashank_24a12res620@iitp.ac.in>
2024-08-29 14:41:59 +0530

    Initial commit updated

---
+++ ch-7/05 for loop iterate.py
@@ -0,0 +1,15 @@
+l = [1, 4, 6, 8, 234, 55, 45] #list
+
+for i in l:
+    print(i)
+    
+t = (1, 4, 6, 8, 234, 55, 45) #tuple
+
+for i in t:
+    print(i)
+    
+s = "Shashank" #string
+
+for i in s:
+    
+    print (i)
